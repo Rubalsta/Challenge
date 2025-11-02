f1a72d4fd12d1e260d351dc8e42ea211408691e7
dandevpy <rubalifemusic@gmal.com>
2025-11-02 09:15:00 -0500

    ALebamic Config, Change to SQLlite for build

--- app/database/session.py
+++ app/database/session.py
@@ -2,7 +2,11 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
 from app.core.config import settings
 
-engine = create_async_engine(settings.database_url, echo=True)
+engine = create_async_engine(
+    settings.DATABASE_URL, 
+    echo=True,
+    connect_args={"check_same_thread": False}
+)
 
 AsyncSessionLocal = sessionmaker(
     engine,
